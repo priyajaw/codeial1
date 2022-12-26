d19269099a31b5c102678c1d15e01c22ece55c78
priya jawariya <priyajaw637@gmail.com>
2022-12-26 15:43:32 +0530

    done with userscontroller

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const router= express.Router();
 const homecontroller= require('../controller/home_controller');
 
 router.get('/', homecontroller.home);
+router.use('/users',require('./users'));
+
 console.log('router loaded');
 module.exports=router;
